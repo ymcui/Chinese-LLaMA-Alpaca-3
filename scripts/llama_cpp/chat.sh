2e9e7e6d9a03d9305aa6731fbbc3cfb96d19afcd
ymcui <16095339+ymcui@users.noreply.github.com>
2024-04-26 12:21:44 +0800

    Create chat.sh

---
+++ scripts/llama_cpp/chat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# script to chat with Llama-3-Chinese-Instruct model
+# usage: ./chat.sh llama-3-chinese-instruct-gguf-model-path your-first-instruction
+# WARNING: the hyperparameters are not optimal, please tune them yourself
+
+FIRST_INSTRUCTION=$2
+SYSTEM_PROMPT="You are a helpful assistant. 你是一个乐于助人的助手。"
+
+./main -m $1 --color -i \
+-c 0 -t 6 --temp 0.2 --repeat_penalty 1.1 -ngl 999 \
+--in-prefix '<|start_header_id|>user<|end_header_id|>\n\n' \
+--in-suffix '<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n' \
+-p "<|start_header_id|>system<|end_header_id|>\n\n$SYSTEM_PROMPT<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n$FIRST_INSTRUCTION<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
