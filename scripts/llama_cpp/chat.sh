db68211a32b48d764f10dfebff497c3a864d1e96
ymcui <16095339+ymcui@users.noreply.github.com>
2024-05-15 14:44:17 +0800

    add reverse prompt for llamacpp chat

--- scripts/llama_cpp/chat.sh
+++ scripts/llama_cpp/chat.sh
@@ -9,6 +9,7 @@ SYSTEM_PROMPT="You are a helpful assistant. 你是一个乐于助人的助手。
 
 ./main -m $1 --color -i \
 -c 0 -t 6 --temp 0.2 --repeat_penalty 1.1 -ngl 999 \
+-r '<|eot_id|>' \
 --in-prefix '<|start_header_id|>user<|end_header_id|>\n\n' \
 --in-suffix '<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n' \
 -p "<|start_header_id|>system<|end_header_id|>\n\n$SYSTEM_PROMPT<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n$FIRST_INSTRUCTION<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
